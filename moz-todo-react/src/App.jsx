f3bab17903887ad7990ec0051d8fdb078297d466
Kanchan Chauhan <kchauhan430@gmail.com>
2024-01-17 14:18:12 +0530

    changes

--- moz-todo-react/src/App.jsx
+++ moz-todo-react/src/App.jsx
@@ -3,11 +3,25 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-const title = "Welcome";
-function App() {
-  return(
-<h1>{title} to my first react App</h1>
-);
-}
+const note = "Welcome";
+const basic = "to our basic react course";
+const moderate = "to our moderate react course";
+const advanced = "to our advanced react course";
+const nametext = "Enter your name";
+const leveltext = "How many years of ecperience do you have?";
+const level1 = "Beginner";
+const level2 = "< 2";
+const level3 = "< 5";
 
+function App(){
+  return (
+    <form>
+      <label>Enter your name:
+      <input type="text" />
+      </label>
+      <label>Enter your name:
+      <input type="radio" />{level1}
+      </label>
+    </form>
+}
 export default App
